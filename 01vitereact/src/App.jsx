63d60c0bc54475e867ce75196307b7e870c2afb8
Harshit Bagga <harshitbagga10@gmail.com>
2024-01-22 15:22:01 +0530

    Code, errors and File structures

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,14 @@
+import Chai from "./chai"
 function App() {
  
 
   return (
+    //<h1>Chai aur React + Vite</h1> we cannot add multiple elements without wrapping it in an empty tag which is <></>
+    <>
     <h1>Chai aur React + Vite</h1>
+    <Chai/>
+    <h3>This is Another element</h3>
+    </>
   )
 }
 
