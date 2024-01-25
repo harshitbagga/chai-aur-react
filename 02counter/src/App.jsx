cc62a2031eafa4e25321873f344777eb0581c393
Harshit Bagga <harshitbagga10@gmail.com>
2024-01-25 14:36:09 +0530

    First mini project with assignment done

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(0)
+  //let counter  = 15
+
+  const addValue = ()=>{
+    //counter  = counter + 1;
+    if(counter == 20 || counter > 20){
+      //counter = 20
+      setCounter(20)
+    } 
+    else{
+      setCounter(counter + 1)
+      console.log("Value added",counter);
+    }
+    
+    
+  }
+
+  const minusvalue = ()=>{
+   
+   if(counter <= 0){
+    counter = 0
+    setCounter(counter)
+   }
+   else{
+    setCounter(counter - 1)
+    console.log("Value decreased", counter);
+   }
+    
+  }
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value {counter}</h2>
+      <button onClick={addValue}>increase Value {counter}</button>
+      <br/>
+      <button onClick={minusvalue}>decrease Value {counter}</button>
+      <p>footer {counter}</p>
+    </>
+  )
+}
+
+export default App
